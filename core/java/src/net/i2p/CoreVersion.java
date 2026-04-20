991cfe199495fbf47073859c88c70987a3814d79
dr|z3d <z3d@i2pmail.org>
2026-04-20 07:38:25 +0000

    Build: fix version regex for static final convention
    - Update getReleaseNumber target to use "public static final" pattern
    - Set PUBLISHED_VERSION to 0.9.68-4+ for I2P+ release"

--- core/java/src/net/i2p/CoreVersion.java
+++ core/java/src/net/i2p/CoreVersion.java
@@ -44,7 +44,7 @@ public class CoreVersion {
      *
      *  @since 0.9.46
      */
-    public static final String PUBLISHED_VERSION = "0.9.68";
+    public static final String PUBLISHED_VERSION = "0.9.68-4+";
 
     /**
      *  For Vuze.

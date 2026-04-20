abaef5b1da09f620339b2cbbffdfcbba458c39a9
dr|z3d <z3d@i2pmail.org>
2026-04-20 20:29:33 +0000

    Router: Add periodic LU router cleanup job

---
+++ router/java/src/net/i2p/router/networkdb/kademlia/CleanupLURoutersJob.java
@@ -0,0 +1,31 @@
+package net.i2p.router.networkdb.kademlia;
+
+import net.i2p.router.JobImpl;
+import net.i2p.router.RouterContext;
+import net.i2p.util.Log;
+
+class CleanupLURoutersJob extends JobImpl {
+    private final Log _log;
+    private final KademliaNetworkDatabaseFacade _facade;
+    private final RouterContext _ctx;
+
+    private static final long INITIAL_DELAY_MS = 60*1000;
+    private static final long RERUN_DELAY_MS = 10*60*1000;
+
+    public CleanupLURoutersJob(RouterContext ctx, KademliaNetworkDatabaseFacade facade) {
+        super(ctx);
+        _ctx = ctx;
+        _log = ctx.logManager().getLog(CleanupLURoutersJob.class);
+        _facade = facade;
+        getTiming().setStartAfter(ctx.clock().now() + INITIAL_DELAY_MS);
+    }
+
+    public String getName() { return "Cleanup LU Routers"; }
+
+    public void runJob() {
+        _log.warn("Running scheduled LU router cleanup...");
+        _facade.cleanupLURouters();
+        getTiming().setStartAfter(_ctx.clock().now() + RERUN_DELAY_MS);
+        _ctx.jobQueue().addJob(this);
+    }
+}

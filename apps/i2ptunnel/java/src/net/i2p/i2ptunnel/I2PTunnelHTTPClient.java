bc959e4608d0ab34beb7a13d2c7bded26971cfb7
dr|z3d <z3d@i2pmail.org>
2026-04-21 17:25:45 +0000

    i2ptunnel: Catch corrupt HTTP responses sooner
    (manual merge of upstream commit 4265d6d)

--- apps/i2ptunnel/java/src/net/i2p/i2ptunnel/I2PTunnelHTTPClient.java
+++ apps/i2ptunnel/java/src/net/i2p/i2ptunnel/I2PTunnelHTTPClient.java
@@ -1148,7 +1148,7 @@ public class I2PTunnelHTTPClient extends I2PTunnelHTTPClientBase implements Runn
                     return;
                 }
                 if (_log.shouldDebug()) {
-                    _log.debug("No HTTP method found in the request.");
+                    _log.debug("No " + (method == null ? "HTTP method" : "destination") + " found in the request");
                 }
                 try {
                     if (protocol != null && "http".equals(protocol.toLowerCase(Locale.US))) {
@@ -1409,11 +1409,24 @@ public class I2PTunnelHTTPClient extends I2PTunnelHTTPClientBase implements Runn
             // this should still be quite effective.
             if (i2ps == null || i2ps.isClosed() ||
                 remotePort != i2ps.getPort() ||
-                !clientDest.equals(i2ps.getPeerDestination())) {
+                !clientDest.equals(i2ps.getPeerDestination()) ||
+                i2ps.getInputStream().available() > 0) {
+
                 if (i2ps != null) {
-                    if (_log.shouldInfo()) {_log.info("Old socket closed or different dest/port, opening new one");}
-                    try {i2ps.close();}
-                    catch (IOException ioe) {}
+                    int avail = i2ps.getInputStream().available();
+                    if (avail > 0) {
+                        // server side bug workaround?
+                        if (_log.shouldWarn()) {
+                            byte[] tmp = new byte[avail];
+                            i2ps.getInputStream().read(tmp);
+                            _log.warn("Resetting old socket with unread data: " + avail + '\n' + net.i2p.util.HexDump.dump(tmp));
+                        }
+                        try { i2ps.reset(); } catch (IOException ioe) {}
+                    } else {
+                        if (_log.shouldInfo())
+                            _log.info("Old socket closed or different dest/port, opening new one");
+                        try { i2ps.close(); } catch (IOException ioe) {}
+                    }
                 }
 
                 Properties opts = new Properties();
